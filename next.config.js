48e88a54349864b2213f1cd6e2d6b3ef498332fe
Particulares2026 <nuriabarbero@icloud.com>
2026-08-22 20:11:57 +0200

    Permitir la lectura local de fotos seleccionadas

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const CSP = [
   "form-action 'self'",
   "frame-ancestors 'self'",
   "frame-src https://challenges.cloudflare.com",
-  `img-src 'self' data: ${SUPABASE_ORIGIN} https://unpkg.com https://*.tile.openstreetmap.org`,
+  `img-src 'self' data: blob: ${SUPABASE_ORIGIN} https://unpkg.com https://*.tile.openstreetmap.org`,
   "font-src 'self'",
   "style-src 'self' 'unsafe-inline'",
   "script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com",
@@ -37,3 +37,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
